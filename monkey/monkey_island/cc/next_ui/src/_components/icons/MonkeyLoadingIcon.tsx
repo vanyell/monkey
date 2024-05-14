b273ed2163c4d2eeb0ca9ff054cedb6a2b77395d
vakarisz <vakarisz@yahoo.com>
2024-05-14 11:00:08 +0300

    UI: Fix loading icon prop types

--- monkey/monkey_island/cc/next_ui/src/_components/icons/MonkeyLoadingIcon.tsx
+++ monkey/monkey_island/cc/next_ui/src/_components/icons/MonkeyLoadingIcon.tsx
@@ -1,9 +1,10 @@
 import Autorenew from '@mui/icons-material/Autorenew';
 import React from 'react';
 import { spinningIcon } from '@/_components/icons/style';
+import { SxProps } from '@mui/material';
 
 type LoadingIconProps = {
-    sx?: React.CSSProperties;
+    sx?: SxProps;
 };
 
 const MonkeyLoadingIcon = (props: LoadingIconProps) => {
